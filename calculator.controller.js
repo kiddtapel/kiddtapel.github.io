046b50e651142d1bc57dab00f210a82c8fef66f5
Kidd Tapel <salvador.tapel@gmail.com>
2020-03-05 22:13:38 +0800

    Added repetitions calculation for war advisor
    Added repetitions calculation for war advisor

--- calculator.controller.js
+++ calculator.controller.js
@@ -40,223 +40,322 @@ angular.module('calcApp', ['ngClipboard'])
             min: esdeathTime,
             max: maxTimeLeft,
             clearedAreas: 3,
-            killsInUncleared: 0
+            killsInUncleared: 0,
+            repetitions: 1
         },{
             title: "Power Overwhelming",
             type: 'Class A Monster',
             min: powerOverwhelming,
             max: esdeathTime,
             clearedAreas: 3,
-            killsInUncleared: 0
+            killsInUncleared: 0,
+            repetitions: 1
         },{
             title: "Full Clear",
             type: "Fast",
             min: fastTime,
             max: powerOverwhelming,
             clearedAreas: 3,
-            killsInUncleared: 0
+            killsInUncleared: 0,
+            repetitions: 1
         },{
             title: "Full Clear",
             type: "Average",
             min: averageTime,
             max: fastTime,
             clearedAreas: 3,
-            killsInUncleared: 0
+            killsInUncleared: 0,
+            repetitions: 1
         },{
             title: "Full Clear",
             type: "Dragged",
             min: foreverTime,
             max: averageTime,
             clearedAreas: 3,
-            killsInUncleared: 0
+            killsInUncleared: 0,
+            repetitions: 1
         },{
             title: "Full Clear",
             type: "Forever",
             min: minTimeLeft,
             max: foreverTime,
             clearedAreas: 3,
-            killsInUncleared: 0
+            killsInUncleared: 0,
+            repetitions: 1
         },{
             title: "2 Clears and 2 Kills",
             type: "Fast",
             min: powerOverwhelming,
             max: maxTimeLeft,
             clearedAreas: 2,
-            killsInUncleared: 2
+            killsInUncleared: 2,
+            repetitions: 1
         },{
             title: "2 Clears and 2 Kills",
             type: "Average",
             min: averageTime,
             max: powerOverwhelming,
             clearedAreas: 2,
-            killsInUncleared: 2
+            killsInUncleared: 2,
+            repetitions: 1
         },{
             title: "2 Clears and 2 Kills",
             type: "Dragged",
             min: foreverTime,
             max: averageTime,
             clearedAreas: 2,
-            killsInUncleared: 2
+            killsInUncleared: 2,
+            repetitions: 1
         },{
             title: "2 Clears and 2 Kills",
             type: "Forever",
             min: minTimeLeft,
             max: foreverTime,
             clearedAreas: 2,
-            killsInUncleared: 2
+            killsInUncleared: 2,
+            repetitions: 1
         },{
             title: "2 Clears and 1 Kill",
             type: "Fast",
             min: powerOverwhelming,
             max: maxTimeLeft,
             clearedAreas: 2,
-            killsInUncleared: 1
+            killsInUncleared: 1,
+            repetitions: 1
         },{
             title: "2 Clears and 1 Kills",
             type: "Average",
             min: averageTime,
             max: powerOverwhelming,
             clearedAreas: 2,
-            killsInUncleared: 1
+            killsInUncleared: 1,
+            repetitions: 1
         },{
             title: "2 Clears and 1 Kill",
             type: "Dragged",
             min: foreverTime,
             max: averageTime,
             clearedAreas: 2,
-            killsInUncleared: 1
+            killsInUncleared: 1,
+            repetitions: 1
         },{
             title: "2 Clears and 1 Kill",
             type: "Forever",
             min: minTimeLeft,
             max: foreverTime,
             clearedAreas: 2,
-            killsInUncleared: 1
+            killsInUncleared: 1,
+            repetitions: 1
         },{
             title: "2 Clears",
             type: "Fast",
             min: powerOverwhelming,
             max: maxTimeLeft,
             clearedAreas: 2,
-            killsInUncleared: 0
+            killsInUncleared: 0,
+            repetitions: 1
         },{
             title: "2 Clears",
             type: "Average",
             min: averageTime,
             max: powerOverwhelming,
             clearedAreas: 2,
-            killsInUncleared: 0
+            killsInUncleared: 0,
+            repetitions: 1
         },{
             title: "2 Clears",
             type: "Dragged",
             min: foreverTime,
             max: averageTime,
             clearedAreas: 2,
-            killsInUncleared: 0
+            killsInUncleared: 0,
+            repetitions: 1
         },{
             title: "2 Clears",
             type: "Forever",
             min: minTimeLeft,
             max: foreverTime,
             clearedAreas: 2,
-            killsInUncleared: 0
+            killsInUncleared: 0,
+            repetitions: 1
         },{
             title: "1 Clear and 2 Kills",
             type: "Fast",
             min: powerOverwhelming,
             max: maxTimeLeft,
             clearedAreas: 1,
-            killsInUncleared: 2
+            killsInUncleared: 2,
+            repetitions: 1
         },{
             title: "1 Clear and 2 Kills",
             type: "Average",
             min: averageTime,
             max: powerOverwhelming,
             clearedAreas: 1,
-            killsInUncleared: 2
+            killsInUncleared: 2,
+            repetitions: 1
         },{
             title: "1 Clear and 2 Kills",
             type: "Dragged",
             min: foreverTime,
             max: averageTime,
             clearedAreas: 1,
-            killsInUncleared: 2
+            killsInUncleared: 2,
+            repetitions: 1
         },{
             title: "1 Clear and 2 Kills",
             type: "Forever",
             min: minTimeLeft,
             max: foreverTime,
             clearedAreas: 1,
-            killsInUncleared: 2
+            killsInUncleared: 2,
+            repetitions: 1
         },{
             title: "1 Clear and 1 Kill",
             type: "Fast",
             min: powerOverwhelming,
             max: maxTimeLeft,
             clearedAreas: 1,
-            killsInUncleared: 1
+            killsInUncleared: 1,
+            repetitions: 1
         },{
             title: "1 Clear and 1 Kills",
             type: "Average",
             min: averageTime,
             max: powerOverwhelming,
             clearedAreas: 1,
-            killsInUncleared: 1
+            killsInUncleared: 1,
+            repetitions: 1
         },{
             title: "1 Clear and 1 Kill",
             type: "Dragged",
             min: foreverTime,
             max: averageTime,
             clearedAreas: 1,
-            killsInUncleared: 1
+            killsInUncleared: 1,
+            repetitions: 1
         },{
             title: "1 Clear and 1 Kill",
             type: "Forever",
             min: minTimeLeft,
             max: foreverTime,
             clearedAreas: 1,
-            killsInUncleared: 1
+            killsInUncleared: 1,
+            repetitions: 1
         },{
             title: "1 Clear",
             type: "Fast",
             min: powerOverwhelming,
             max: maxTimeLeft,
             clearedAreas: 1,
-            killsInUncleared: 0
+            killsInUncleared: 0,
+            repetitions: 1
         },{
             title: "1 Clear",
             type: "Average",
             min: averageTime,
             max: powerOverwhelming,
             clearedAreas: 1,
-            killsInUncleared: 0
+            killsInUncleared: 0,
+            repetitions: 1
         },{
             title: "1 Clear",
             type: "Dragged",
             min: foreverTime,
             max: averageTime,
             clearedAreas: 1,
-            killsInUncleared: 0
+            killsInUncleared: 0,
+            repetitions: 1
         },{
             title: "1 Clear",
             type: "Forever",
             min: minTimeLeft,
             max: foreverTime,
             clearedAreas: 1,
-            killsInUncleared: 0
+            killsInUncleared: 0,
+            repetitions: 1
         }, {
             title: "2 Kills",
             min: 0,
             max: 0,
             clearedAreas: 0,
-            killsInUncleared: 2
+            killsInUncleared: 2,
+            repetitions: 1
         }, {
             title: "1 Kill",
             min: 0,
             max: 0,
             clearedAreas: 0,
-            killsInUncleared: 1
+            killsInUncleared: 1,
+            repetitions: 1
         }];
+
+        calculator.getCalculatorHref = function(instructions) {
+            var string = "index.html?tier=" + calculator.tier + "&";
+            for(var i = 0; i < 3; i++) {
+                if (instructions[i]) {
+                    string += "clearTime." + i + "=" + (instructions[i].clearTime || "0") + "&";
+                    string += "kills." + i + "=" + (instructions[i].kills || "0") + "&";
+                    string += "score." + i + "=" + (instructions[i].score || "0") + "&";
+                }
+            }
+            return string;
+        };
+
+        calculator.getAdviceByScore = function(targetScore){
+            calculator.recommendations = [];
+            calculator.foundAdvice = false;
+
+            cases.forEach(function(e){
+                var minScore = calculator.getBoundary(e.clearedAreas, e.min, e.killsInUncleared);
+                var maxScore = calculator.getBoundary(e.clearedAreas, e.max, e.killsInUncleared);
+                t = targetScore / e.repetitions;
+                console.log(e.title, e.repetitions, "repetitions", t, minScore, maxScore, t > minScore && t <= maxScore);
+                e.show = t > minScore && t <= maxScore;
+                if (e.show) calculator.foundAdvice = true;
+            });
+            return cases;
+        };
+
+        calculator.getBoundary = function(numberOfAreasCleared, timeLeft, killsInAreaUncleared) {
+            return (killScore * 3 + (scorePerSecond * (timeLeft))) * numberOfAreasCleared + ((killsInAreaUncleared || 0) * killScore);
+        };
+
+        calculator.getInstructionsFromStrategy = function(strategy, targetScore) {
+            let score = Math.floor(targetScore / strategy.repetitions);
+            if (strategy.killsInUncleared) {
+                score -= strategy.killsInUncleared * killScore;
+            }
+            if (strategy.clearedAreas) {
+                score /= strategy.clearedAreas;
+            }
+            var areas = [];
+            var area = calculator.computeByScore(score);
+            for(var i = 0; i < strategy.clearedAreas; i++) {
+                areas.push({
+                    clearTime: area.clearTime,
+                    kills: area.kills,
+                    score: area.score
+                });
+            }
+            if (strategy.killsInUncleared) {
+                areas.push({
+                    clearTime: 0,
+                    kills: strategy.killsInUncleared,
+                    score: strategy.killsInUncleared*killScore
+                })
+            }
+            var length = areas.length;
+            for(var j = 0; j < 3-length; j++) {
+                console.log('push empty instruction', JSON.stringify(strategy), j, 3-areas.length);
+                areas.push({
+                    clearTime: 0,
+                    kills: 0,
+                    score: 0
+                });
+            }
+            return areas;
+        };
 
         calculator.tier = "1";
         var maxAreaScore = 3600, scorePerSecond = 10.5, killScore = 780;
@@ -379,72 +478,6 @@ angular.module('calcApp', ['ngClipboard'])
             return area;
         };
 
-        calculator.getCalculatorHref = function(instructions) {
-            var string = "index.html?";
-            for(var i = 0; i < 3; i++) {
-                if (instructions[i]) {
-                    string += "clearTime." + i + "=" + (instructions[i].clearTime || "0") + "&";
-                    string += "kills." + i + "=" + (instructions[i].kills || "0") + "&";
-                    string += "score." + i + "=" + (instructions[i].score || "0") + "&";
-                }
-            }
-            return string;
-        };
-
-        calculator.getAdviceByScore = function(targetScore){
-            calculator.recommendations = [];
-            calculator.foundAdvice = false;
-
-            cases.forEach(function(e){
-                var minScore = calculator.getBoundary(e.clearedAreas, e.min, e.killsInUncleared);
-                var maxScore = calculator.getBoundary(e.clearedAreas, e.max, e.killsInUncleared);
-                console.log(targetScore, minScore, maxScore, targetScore > minScore && targetScore <= maxScore);
-                e.show = targetScore > minScore && targetScore <= maxScore;
-                if (e.show) calculator.foundAdvice = true;
-            });
-            return cases;
-        };
-
-        calculator.getBoundary = function(numberOfAreasCleared, timeLeft, killsInAreaUncleared) {
-            return (killScore * 3 + (scorePerSecond * (timeLeft))) * numberOfAreasCleared + ((killsInAreaUncleared || 0) * killScore);
-        };
-
-        calculator.getInstructionsFromStrategy = function(strategy, targetScore) {
-            let score = targetScore;
-            if (strategy.killsInUncleared) {
-                score -= strategy.killsInUncleared * killScore;
-            }
-            if (strategy.clearedAreas) {
-                score /= strategy.clearedAreas;
-            }
-            var areas = [];
-            var area = calculator.computeByScore(score);
-            for(var i = 0; i < strategy.clearedAreas; i++) {
-                areas.push({
-                    clearTime: area.clearTime,
-                    kills: area.kills,
-                    score: area.score
-                });
-            }
-            if (strategy.killsInUncleared) {
-                areas.push({
-                    clearTime: 0,
-                    kills: strategy.killsInUncleared,
-                    score: strategy.killsInUncleared*killScore
-                })
-            }
-            var length = areas.length;
-            for(var j = 0; j < 3-length; j++) {
-                console.log('push empty instruction', JSON.stringify(strategy), j, 3-areas.length);
-                areas.push({
-                    clearTime: 0,
-                    kills: 0,
-                    score: 0
-                });
-            }
-            return areas;
-        };
-
         calculator.total = function () {
             var total = 0, end = false;
             calculator.areas.forEach(function (a) {
@@ -471,17 +504,23 @@ angular.module('calcApp', ['ngClipboard'])
                 }
             });
 
-            str += `Score: ${ areas.reduce(function(total, e){ return total + e.score; }, 0) }`;
+            str += `Score: ${ areas.reduce(function(total, e){ return total + e.score; }, 0) }. `;
+
             return str;
         };
 
-        calculator.copyInstructions = function(strategy) {
+        calculator.copyInstructions = function(strategy, repetitions) {
 
             var areas = calculator.areas;
             if (strategy) {
                 areas = strategy;
             }
             str = calculator.getInstructionsToString(areas);
+
+            console.log(repetitions);
+            if (parseInt(repetitions) > 1) {
+                str += repetitions + " times. Total Score: " + (areas.reduce(function(total, e){ return total + e.score; }, 0) * repetitions);
+            }
 
             console.log(str);
             ngClipboard.toClipboard(str);
@@ -497,5 +536,19 @@ angular.module('calcApp', ['ngClipboard'])
                 calculator.recompute(parseInt(keys[1]), keys[0]);
             }
         });
+
+        if (['1','2','3'].indexOf(params.tier) > -1) {
+            calculator.tier = params.tier;
+            calculator.changeTier();
+        }
+
+        for (var reps = 2; reps <= 3; reps++)
+        cases.forEach(function(c) {
+            if (c.repetitions === 1) {
+                let clone = JSON.parse(JSON.stringify(c));
+                clone.repetitions = reps;
+                cases.push(clone);
+            }
+        })
 
     });
